b6146c6e8f9de92a7e57937bfcb9568649cbd1cc
Ethan Bourguigneau <ethan.devfs@pm.me>
2026-07-04 18:34:19 +0200

    feat : ajout favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Mon classement d'écoles",
   description: "Classez vos écoles par ordre de préférence, exportez votre voeux.",
+    icons: {
+        icon: "/favicon.ico",
+    },
 };
 
 export default function RootLayout({
